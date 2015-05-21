b83c1d44d7e4248848c0e7d4ce6adcbd55c59ea0
David Kunzmann <davidkunzmann@gmail.com>
2015-05-21 16:08:32 +0200

    Backend Frontend communication

--- front_end/frontendapp/index.php
+++ front_end/frontendapp/index.php
@@ -1,1 +1,6 @@
-<?php echo "<h1>Hello World</h1>" ;
+<?php 
+
+$homepage = file_get_contents('http://172.17.0.7:3000/message');
+echo $homepage;
+echo "<h1>Hello World</h1>" ;
+// var_dump(json_decode($json));
