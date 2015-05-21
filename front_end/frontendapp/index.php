28081d5f993cb6c7acf2befa9cb076b6eed70a2a
David Kunzmann <davidkunzmann@gmail.com>
2015-05-21 21:47:11 +0200

    Changing message from backend

--- front_end/frontendapp/index.php
+++ front_end/frontendapp/index.php
@@ -1,6 +1,6 @@
 <?php 
 
-$homepage = file_get_contents('http://172.17.0.7:3000/message');
+$homepage = file_get_contents('http://172.17.0.4:3000/message');
 echo $homepage;
 echo "<h1>Hello World</h1>" ;
 // var_dump(json_decode($json));
